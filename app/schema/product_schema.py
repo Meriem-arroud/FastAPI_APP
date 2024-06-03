b8de5bc975e61cb6b3b2572ace8cbc0407c763e2
Meriem Arroud <Meriem.Arroud@ibm.com>
2024-06-03 15:47:28 +0100

    add product schema

---
+++ app/schema/product_schema.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, HttpUrl
+from typing import Optional
+
+class ProductCreate(BaseModel):
+    title: str
+    imgUrl: HttpUrl
+    price: float
+    category_id: int
+
+
+class ProductUpdate(BaseModel):
+    title: Optional[str] = None
+    imgUrl: Optional[str] = None
+    stars: Optional[float] = 0
+    reviews: Optional[int] = 0
+    price: Optional[float] = 0
+    category_id: Optional[int] = None
+    isBestSeller: Optional[bool] = False    
+
+    class Config:
+        orm_mode = True
